db4dcd9ee2812c2fd6b213f3f29fbbdbfca4a5e3
NiemTT <trieuniemit@gmail.com>
2019-12-26 17:52:24 +0700

    up

--- main.go
+++ main.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world")
+	arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for index, number := range arr {
+		fmt.Println(isPrime(index, number))
+	}
+}
+
+func isPrime(index int, number int) (int, bool) {
+	for i := 2; i < number; i++ {
+		if number%i == 0 {
+			return number, false
+		}
+	}
+
+	return number, true
 }
