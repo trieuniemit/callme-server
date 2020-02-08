124304609f24d3fcdbb9cf0ea0cd0928255a47ec
NiemTT <trieuniemit@gmail.com>
2020-02-08 10:39:47 +0700

    update env production

--- script/migrate/migration.go
+++ script/migrate/migration.go
@@ -1,11 +1,9 @@
 package migrate
 
 import (
-	"webrtc-server/internal/models"
-
 	"github.com/jinzhu/gorm"
 )
 
 func Migrate(db *gorm.DB) {
-	db.Debug().AutoMigrate(&models.Error{}, &models.Todo{})
+	// db.Debug().AutoMigrate(&models.Error{}, &models.Todo{})
 }
